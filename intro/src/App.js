8ff81f5c0938fc63a334be02d33627c856d76cff
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2021-11-18 13:17:34 +0300

    dosya eklendi

---
+++ intro/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+
+
+
+function App() {
+  return (
+    <div >
+      <Navi></Navi>
+      <CategoryList/>
+      <ProductList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ intro/src/Navi.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Navi extends Component {  //navi artık bir componenttir
+  render() { //componenti değişe veriye göre tekrardan çalıştırmaya yarar
+    return ( 
+    <div>
+         <h2>Navi Component</h2>
+    </div>
+    )
+  }
+}
