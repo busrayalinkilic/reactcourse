f338a234e88fa6880db78da7a72fcf6812dca114
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2021-11-22 13:35:16 +0300

    state

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -1,21 +1,26 @@
 import React, { Component } from "react";
-import {ListGroup, ListGroupItem} from "reactstrap";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-     constructor(props){
-          super(props);
-          state:{}
-     }
+//   constructor(props) {
+//     super(props);
+//     this.state
+     state = {
+      categories: [
+        { categoryId: 1, categoryName: "Beverages" },
+        { categoryId: 2, categoryName: "Condiments" },
+      ],
+    };
+  
   render() {
     return (
       <div>
-           <h2>{this.props.info.title}</h2>
+        <h2>{this.props.info.title}</h2>
+
         <ListGroup>
-          <ListGroupItem color="success">Chlotes</ListGroupItem>
-          <ListGroupItem color="info">Shoes</ListGroupItem>
-          <ListGroupItem color= "warning">Fruits</ListGroupItem>
-          <ListGroupItem color= "danger">Snacks</ListGroupItem>
-          <ListGroupItem color= "primary">Breads</ListGroupItem>
+          {this.state.categories.map((category) => (
+            <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
+          ))}
         </ListGroup>
       </div>
     );
--- intro/src/ProductList.js
+++ intro/src/ProductList.js
@@ -2,10 +2,10 @@ import React, { Component } from "react"
 
 
 export default class ProductList extends Component{
-     constructor(props){
-          super(props);
-          state:{}
-     }
+     // constructor(props){
+     //      super(props);
+     //      state:{}
+     // }
      render(){
           return(
                <div>
