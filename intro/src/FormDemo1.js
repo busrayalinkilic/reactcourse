e00faf6442fb4afc2d9ea4b8834c39804718fcb2
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2021-11-27 19:09:38 +0300

    çokluForm

--- intro/src/FormDemo1.js
+++ intro/src/FormDemo1.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
 class FormDemo1 extends Component {
-     state = {userName: ""}
+     state = {userName: "", city: ""}
      onChangeHandler=(event)=>{
-          this.setState({userName:event.target.value})
+          //this.setState({userName:event.target.value})
+          let name =event.target.name;
+          let value = event.target.value;
+          this.setState({[name]: value})
      }
      onSubmitHandler= (event)=>{
           event.preventDefault();
@@ -14,9 +17,16 @@ class FormDemo1 extends Component {
                <div>
                     <form on onSubmit={this.onSubmitHandler}>
                        <h3>User Name</h3>  
-                       <input onChange={this.onChangeHandler} type="text"></input>
-                       <h3>User Name is {this.state.userName}</h3>  
+                       <input name="userName" onChange={this.onChangeHandler} type="text"></input>
+                       <h3>User Name is {this.state.userName}</h3> 
+
+                       <h3>City</h3>  
+                       <input name ="city" onChange={this.onChangeHandler} type="text"></input>
+                       <h3>City is {this.state.city}</h3>
+                       
+                        
                        <input type="submit" value="Save"></input>
+                      
                     </form>
                     
                </div>
