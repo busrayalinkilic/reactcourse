c70d1d7f08ab37d0ef7bc820fb54ecd4eacfb1dd
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2021-11-27 18:41:06 +0300

    formDemo1

--- intro/src/Navi.js
+++ intro/src/Navi.js
@@ -9,6 +9,7 @@ import {
   NavLink,
 } from "reactstrap";
 import CartSummary from "./CartSummary";
+import { Link } from "react-router-dom";
 
 export default class Navi extends React.Component {
   constructor(props) {
@@ -33,7 +34,7 @@ export default class Navi extends React.Component {
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/components/">Components</NavLink>
+                <Link to="form1">Form Demo 1</Link>
               </NavItem>
               <NavItem>
                 <NavLink href="https://github.com/reactstrap/reactstrap">
