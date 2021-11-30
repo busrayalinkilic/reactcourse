47bde2e8756ea44dc7b18a488015a0fccd5d2091
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2021-11-30 17:21:22 +0300

    connect

--- northwind-redux/src/components/categories/CategoryList.js
+++ northwind-redux/src/components/categories/CategoryList.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
-
-export default class CategoryList extends Component {
+import { connect } from 'react-redux'
+ class CategoryList extends Component {
      render() {
           return (
                <div>
-                  <h3>Category</h3>  
+                  <h3>Categories</h3>  
+                  <h5>Seçili Kategori : {this.props.currentCategory.categoryName}</h5>
                </div>
           )
      }
 }
+
+function mapStateToProps(state){
+     return{
+          currentCategory:state.changeCategoryReducer
+     }
+}
+
+export default connect(mapStateToProps)(CategoryList)
