d47a728481f35b23dca639fc925a757acbc93573
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2021-12-03 09:36:24 +0300

    kategori listelendi

--- northwind-redux/src/components/categories/CategoryList.js
+++ northwind-redux/src/components/categories/CategoryList.js
@@ -1,30 +1,46 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import * as categoryActions from '../../redux/actions/categoryActions'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import { bindActionCreators } from "redux";
+import * as categoryActions from "../../redux/actions/categoryActions";
 
- class CategoryList extends Component {
-     render() {
-          return (
-               <div>
-                  <h3>Categories</h3>  
-                  <h5>Seçili Kategori : {this.props.currentCategory.categoryName}</h5>
-               </div>
-          )
-     }
+class CategoryList extends Component {
+  componentDidMount() {
+    this.props.actions.getCategories();
+  }
+  render() {
+    return (
+      <div>
+        <h3>Categories </h3>
+
+        <ListGroup>
+          {this.props.categories.map((category) => (
+            <ListGroupItem key={category.id}>
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+        <h5>Seçili Kategori : {this.props.currentCategory.categoryName}</h5>
+      </div>
+    );
+  }
 }
 
-function mapStateToProps(state){
-     return{
-          currentCategory:state.changeCategoryReducer
-     }
+function mapStateToProps(state) {
+  return {
+    currentCategory: state.changeCategoryReducer,
+    categories: state.categoryListReducer,
+  };
 }
-function mapDispatchToProps(dispatch){
-     return{
-          actions:{
-               getCategories:bindActionCreators(categoryActions.getCategories,dispatch)
-          }
-     }
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getCategories: bindActionCreators(
+        categoryActions.getCategories,
+        dispatch
+      ),
+    },
+  };
 }
 
-export default connect(mapStateToProps)(CategoryList)
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
