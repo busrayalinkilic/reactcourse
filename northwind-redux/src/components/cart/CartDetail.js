f0c5d4a7a78d1b6dc2c8c0dfde037de952189aeb
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2021-12-06 17:27:34 +0300

    routing

---
+++ northwind-redux/src/components/cart/CartDetail.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import * as cartActions from "../../redux/actions/cartActions";
+
+class CartDetail extends Component {
+  render() {
+    return <div>
+         <h1>sepet</h1>
+    </div>;
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      removeFromCart: bindActionCreators(cartActions.removeFromCart, dispatch),
+    },
+  };
+}
+function mapStateToProps(state) {
+  return {
+    cart: state.cartReducer,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartDetail);
--- northwind-redux/src/components/root/App.js
+++ northwind-redux/src/components/root/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Route, Switch } from "react-router";
 import { Container } from "reactstrap";
+import CartDetail from "../cart/CartDetail";
 import Navi from "../navi/Navi";
 import Dashboard from "./Dashboard";
 
 function App() {
   return <Container>
     <Navi/>
-    <Dashboard/>
+    <Switch>
+      <Route path="/" exact component={Dashboard}/>
+      <Route path="/product" exact component={Dashboard}/>
+      <Route path="/cart" exact component={CartDetail}/>
+
+      
+    </Switch>
+    
   </Container>;
 }
 
